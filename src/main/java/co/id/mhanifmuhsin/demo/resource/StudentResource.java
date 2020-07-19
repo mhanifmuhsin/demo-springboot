d9316dcc7f10cf3113f01f31bd9efc2b6d6f0df4
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2020-07-19 15:54:57 +0700

    final demo

--- src/main/java/co/id/mhanifmuhsin/demo/resource/StudentResource.java
+++ src/main/java/co/id/mhanifmuhsin/demo/resource/StudentResource.java
@@ -4,10 +4,7 @@ import co.id.mhanifmuhsin.demo.model.Student;
 import co.id.mhanifmuhsin.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -31,5 +28,20 @@ public class StudentResource {
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void insertStudent(@RequestBody Student student) {
         studentService.insertNewStudent(UUID.randomUUID(), student);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, path = "{studentId}")
+    public Student getStudentbyId(@PathVariable("studentId") UUID studentId){
+        return studentService.selectStudentById(studentId);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,path = "{studentId}")
+    public void deleteStudentById(@PathVariable("studentId") UUID studentId){
+        studentService.deleteStudentById(studentId);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") UUID studentId, @RequestBody Student student){
+        studentService.updateStudentById(studentId, student);
     }
 }
